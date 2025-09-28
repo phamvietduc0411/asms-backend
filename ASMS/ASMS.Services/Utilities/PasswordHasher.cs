986e467bc038a270268409450f00222b51217e25
phamvietduc0411 <phamvietduc0411@gmail.com>
2025-09-28 20:52:24 +0700

    add hasing password function

---
+++ ASMS/ASMS.Services/Utilities/PasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿using Isopoh.Cryptography.Argon2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMS.Services.Utilities
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password) => Argon2.Hash(password);
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+            => Argon2.Verify(hashedPassword, password);
+
+    }
+}
