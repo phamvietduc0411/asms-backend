afeb485f2ad6c825fea592eb8960e50f722dbc90
Thanh3Bien <thanhlv1793@gmail.com>
2025-10-06 12:01:14 +0700

    Rerun-DB

--- ASMS/ASMS.Repositories/Data/AsmsContext.cs
+++ ASMS/ASMS.Repositories/Data/AsmsContext.cs
@@ -1,8 +1,7 @@
-﻿using ASMS.Repositories.Entities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using ASMS.Repositories.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASMS.Repositories.Data;
 
@@ -50,15 +49,8 @@ public partial class AsmsContext : DbContext
     public virtual DbSet<WorkflowTemplate> WorkflowTemplates { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    { optionsBuilder.UseSqlServer(GetConnectionString()); }
-
-    private string GetConnectionString()
-    {
-        IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true, true).Build();
-        return configuration["ConnectionStrings:DefaultConnection"];
-    }
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-39B7IASC\\SQLEXPRESS;Database=ASMS;Uid=sa;Pwd=1;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
