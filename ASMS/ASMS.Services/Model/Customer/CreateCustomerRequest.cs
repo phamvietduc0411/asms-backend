473384f1f8982ed7ac1ca98d3096cc95cede8c86
Pham Viet Duc <phamvietduc0411@gmail.com>
2025-11-30 15:31:28 +0700

    Assign delivery for new order

--- ASMS/ASMS.Services/Model/Customer/CreateCustomerRequest.cs
+++ ASMS/ASMS.Services/Model/Customer/CreateCustomerRequest.cs
@@ -4,16 +4,16 @@
     {
         public string CustomerCode { get; set; } = null!;
 
-        public string? Phone { get; set; }
+        public string Phone { get; set; }
 
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         public bool? IsActive { get; set; }
 
         public string? Address { get; set; }
 
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
-        public string? Password { get; set; }
+        public string Password { get; set; }
     }
 }
